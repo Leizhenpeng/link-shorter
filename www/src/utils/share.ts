fe95dbb42244256dacf28cdf1588e23b1bdb07e2
river <laolei@forkway.cn>
2023-01-20 08:46:07 +0800

    feat(www): window can popup in center of screen

--- www/src/utils/share.ts
+++ www/src/utils/share.ts
@@ -6,11 +6,17 @@ export function ShareTwitter(url: string) {
   params.set("hashtags", "LinkShorter");
 
   const twitterUrl = `https://twitter.com/share?${ params.toString() }`;
-  window.open(twitterUrl, "", "left=0,top=auto,width=550,height=450");
+  popupWindow(twitterUrl, "Share on Twitter", window, 800, 600);
 }
 
 
 export function GenQrcode() {
   const twitterUrl = `https://cli.im/url`;
-  window.open(twitterUrl, "", "left=0,top=auto,width=1100,height=500");
+  popupWindow(twitterUrl, "", window, 1180, 500);
+}
+
+function popupWindow(url: string, windowName: string, win: any, w: number, h: number) {
+  const y = win.top.outerHeight / 2 + win.top.screenY - (h / 2);
+  const x = win.top.outerWidth / 2 + win.top.screenX - (w / 2);
+  return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${ w }, height=${ h }, top=${ y }, left=${ x }`);
 }
